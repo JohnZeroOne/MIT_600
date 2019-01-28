7c1d708d7b0244ae6a07f9af1dfa667a0cee552b
Murray <42144237+JohnZeroOne@users.noreply.github.com>
2019-01-28 11:50:29 +0000

    ps0
    Prints your name and DOB in Python 2.x.x

---
+++ Problem Set 0/ps0.py
@@ -0,0 +1,13 @@
+# Problem Set 0 
+# Name: John Smith
+# Collaborators: N/A
+# Time Spent: 0:10
+
+#Person details printer
+#Asks for date of birth
+#Asks for last name
+#Print last name then DOB
+
+dob = str(raw_input('Enter your date of birth:'))
+lastname = str(raw_input('Enter your surname:'))
+print lastname, dob
